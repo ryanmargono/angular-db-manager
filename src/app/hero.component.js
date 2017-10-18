3b5ec7ee3ca2526a12464c1744db5ea59f29b8c9
Ryan Margono <rtm308@nyu.edu>
2017-10-18 10:13:10 -0400

    first commit

---
+++ src/app/hero.component.js
@@ -0,0 +1,68 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
+var hero_service_1 = require("./hero.service");
+var HeroComponent = (function () {
+    function HeroComponent(heroService, router) {
+        this.heroService = heroService;
+        this.router = router;
+    }
+    HeroComponent.prototype.onSelect = function (hero) {
+        this.selectedHero = hero;
+    };
+    HeroComponent.prototype.getHeroes = function () {
+        var _this = this;
+        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
+    };
+    HeroComponent.prototype.ngOnInit = function () {
+        this.getHeroes();
+    };
+    HeroComponent.prototype.gotoDetail = function () {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    };
+    HeroComponent.prototype.add = function (name) {
+        var _this = this;
+        name = name.trim();
+        if (!name) {
+            return;
+        }
+        this.heroService.create(name)
+            .then(function (hero) {
+            _this.heroes.push(hero);
+            _this.selectedHero = null;
+        });
+    };
+    HeroComponent.prototype.delete = function (hero) {
+        var _this = this;
+        this.heroService
+            .delete(hero.id)
+            .then(function () {
+            _this.heroes = _this.heroes.filter(function (h) { return h !== hero; });
+            if (_this.selectedHero === hero) {
+                _this.selectedHero = null;
+            }
+        });
+    };
+    return HeroComponent;
+}());
+HeroComponent = __decorate([
+    core_1.Component({
+        selector: 'my-heroes',
+        templateUrl: './hero.component.html',
+        styleUrls: ['./hero.component.css'],
+        providers: [hero_service_1.HeroService]
+    }),
+    __metadata("design:paramtypes", [hero_service_1.HeroService,
+        router_1.Router])
+], HeroComponent);
+exports.HeroComponent = HeroComponent;
+//# sourceMappingURL=hero.component.js.map
